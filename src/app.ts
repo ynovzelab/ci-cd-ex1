4d7acc424ae2e5fa29592ee78b835db8c1db571e
Vincent Zerbib <contact@zelabstudio.com>
2025-04-14 15:49:36 +0200

    save work

--- src/app.ts
+++ src/app.ts
@@ -29,6 +29,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => {
+server.listen(process.env.PORT, () => {
   console.log("Server is running on port 3000");
 });
