25738e185f2f483a2de4bb1ab1797d699a5bf63a
Vincent Zerbib <contact@zelabstudio.com>
2025-04-15 10:24:16 +0200

    fix prettier

--- src/lib/index.test.ts
+++ src/lib/index.test.ts
@@ -2,27 +2,27 @@ import { displayUserName } from "./index";
 import { User } from "../interfaces/";
 
 describe("displayUserName", () => {
-    it("should return the full name of the user", () => {
-        const user:User = { firstName: "John", lastName: "Doe" };
-        const result = displayUserName(user);
-        expect(result).toBe("John Doe");
-    });
+  it("should return the full name of the user", () => {
+    const user: User = { firstName: "John", lastName: "Doe" };
+    const result = displayUserName(user);
+    expect(result).toBe("John Doe");
+  });
 
-    it("should handle empty firstName and lastName", () => {
-        const user = { firstName: "", lastName: "" };
-        const result = displayUserName(user);
-        expect(result).toBe(" ");
-    });
+  it("should handle empty firstName and lastName", () => {
+    const user = { firstName: "", lastName: "" };
+    const result = displayUserName(user);
+    expect(result).toBe(" ");
+  });
 
-    it("should handle missing firstName", () => {
-        const user = { firstName: "", lastName: "Doe" };
-        const result = displayUserName(user);
-        expect(result).toBe(" Doe");
-    });
+  it("should handle missing firstName", () => {
+    const user = { firstName: "", lastName: "Doe" };
+    const result = displayUserName(user);
+    expect(result).toBe(" Doe");
+  });
 
-    it("should handle missing lastName", () => {
-        const user = { firstName: "John", lastName: "" };
-        const result = displayUserName(user);
-        expect(result).toBe("John ");
-    });
+  it("should handle missing lastName", () => {
+    const user = { firstName: "John", lastName: "" };
+    const result = displayUserName(user);
+    expect(result).toBe("John ");
+  });
 });
